d01a238776fd073524888a224b315d5bdc2b40a2
Abba00 <aaliconcern@gmail.com>
2023-12-22 00:30:30 +0100

    DOM completed

---
+++ DOM/index.js
@@ -0,0 +1,6 @@
+// // alert("Hello from external")
+// // document.querySelector("ul").lastElementChild.innerHTML = "Abba"
+// // console.log(li[0])
+// // document.querySelector("h1").innerHTML = "Good Bye";
+// // document.querySelector("li a").style.color = "red";
+// document.querySelector("button").style.backgroundColor= "yellow"
--- Javascript/BMICalculator.js
+++ Javascript/BMICalculator.js
@@ -1,11 +1,3 @@
-// // // var weight = prompt("Enter your weight:")
-// // // var height = prompt("Enter your height:")
-
-// // // function bmi(height, weight){
-// // //     return (weight/(height*height));
-// // // }
-// // // console.log("Your BMI index is " + bmi)
-
 // // output = [0, 2, 6, 7]
 // // output.pop()
 // // console.log(output)
@@ -18,21 +10,13 @@
 
 // WhosBuyingLunch(names)
 
+var weight = prompt("Enter your weight:")
+var height = prompt("Enter your height:")
 
-function fibonacciGenerator(n){
-    output = []
-    if (n === 1){
-        output = [0]
-    }
-    else if (n === 2){
-        output = [0, 1]
-    }
-    else {
-        output = [0, 1]
-        for (var i = 2; i < n; i++){
-            output.push(output[i-2] + output[i-1])
-        }
-    }
-    return output;
+function bmi(height, weight){
+    return (weight/(height*height));
 }
-console.log(fibonacciGenerator(8))
+console.log("Your BMI index is " + bmi)
+
+
+
