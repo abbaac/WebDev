d72ab5b1ed3d7175a8caf512dc95b4e45cbedc68
Abba00 <aaliconcern@gmail.com>
2024-02-26 20:39:54 +0100

    setup + req/res basics

--- Backend-Projects/my-express-server/server.js
+++ Backend-Projects/my-express-server/server.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express(); // Using the express module
 
+// defines what should happen when get request is made to the home (/) root.
+// after a callback function tells the server what to do when a request is made.
+
+app.get("/", function(request, response){   // can be req, res
+    // console.log(request);
+    response.send("<h1>Hello, World!    </h1>")
+});
+
+app.get("/contact", function(req, res){
+    res.send("<p>My email is aaliconcern@gmail.com</p>")
+})
+
+app.get("/about", function(req, res){
+    res.send("<h1>Hi, my name is Abba</h1> <h2>I am an aspiring Data Scientist and Software Developer.</h2>")
+})
+
 app.listen(3000, function(){    //Listen on speciffc port for nay HTTP requests 
     console.log("Server has started on port 3000.")
 }); 
